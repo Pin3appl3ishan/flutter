bbc1b14e72a7cdde4ff3b80f861a19ff80965490
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-05-06 17:27:01 +0545

    feat: Update main app to use BottomNavigationBarExample instead of FirstView

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:sem5/view/first_view.dart';
+// import 'package:sem5/view/first_view.dart';
+import 'view/bottom_navigation_bar.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: FirstView());
+    return MaterialApp(home: BottomNavigationBarExample());
   }
 }
